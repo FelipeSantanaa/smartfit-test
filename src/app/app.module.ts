c17963971c9bf114bdcac103e44564228442a57c
Felipe <felipe008_@hotmail.com>
2023-10-13 13:20:09 -0300

    form responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,7 +14,8 @@ import { FormsComponent } from './components/forms/forms.component';
     FormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.css']
+  styleUrls: ['./forms.component.css'],
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+  results = [];
+  formGroup!: FormGroup;
 
-  results = []
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit() {
+    this.formGroup = this.fb.group({
+      hour: '',
+      showClosed: false
+    })
+  }
 
+  onSubmit(){
+    console.log('Submit', this.formGroup.value)
+  }
+  
+  onClean(){
+    this.formGroup.reset();
+  }
 }
