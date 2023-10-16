c46c7eb4551e211f81516ee4b3ec0d90322cb83b
Felipe <felipe008_@hotmail.com>
2023-10-16 18:42:49 -0300

    filter units open/closed

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -2,26 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from 'src/app/services/get-units.service';
 
+import { Location } from 'src/app/types/location.interface';
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css'],
 })
 export class FormsComponent implements OnInit {
-  results = [];
+  results: Location[] = [];
+  filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
   constructor(private fb: FormBuilder, private unitService: GetUnitsService) {}
 
   ngOnInit() {
-    this.unitService.getAllUnits().subscribe(data => console.log(data));
+    this.unitService.getAllUnits().subscribe(data => {
+      this.results = data.locations;
+      this.filteredResults = data.locations;
+    });
     this.formGroup = this.fb.group({
       hour: '',
-      showClosed: false
+      showClosed: true
     })
   }
 
   onSubmit(){
+    if(!this.formGroup.value.showClosed){
+      this.filteredResults = this.results.filter(location => location.opened == true)
+    } else {
+      this.filteredResults = this.results
+    }
+
+
     console.log('Submit', this.formGroup.value)
   }
   
